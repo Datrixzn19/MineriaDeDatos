ce887b8e381af5808a163be7cb4fd3c3c0f8c67b
David19 <davidleonel911@gmail.com>
2026-06-06 21:01:15 -0500

    basico

--- Basico/1-Introduccion.py
+++ Basico/1-Introduccion.py
@@ -1,5 +1,8 @@
 import pandas as pd #importanmos la libreria pandas y le ponemos el apodo pd
 
-df = pd.read_csv('1-Introduccion.py')
+df = pd.read_csv('Basico/dataset.csv')#lo convertimos a dataframe 
+#
 
-print()
+print(df.head(2))#muestra la cantidad de filas que le indiques
+
+print(df.info())#muestra informacion general del dataframe
