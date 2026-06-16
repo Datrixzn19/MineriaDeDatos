765fa6a0f7c6380985f0d199db8de59fc5b1661a
David19 <davidleonel911@gmail.com>
2026-06-15 19:45:03 -0500

    imprimir columnas

--- Basico/2.py
+++ Basico/2.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv('Basico/fifa_players.csv')
-# print(df.info())
+df = pd.read_csv('Basico/fifa_players.csv', encoding='latin1') #latin1 para tildes y ñ
+
 
 #print(df.isnull().sum()) #sumar todos los valores nulos de cada columna
 
 print(df['age'].mean()) 
 
 nacionalidad = df[df['nationality'] == 'Ecuador']
-print(nacionalidad['birth_date'])
+
+print(nacionalidad['name'])
