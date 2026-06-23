9e1fa10066b871f370ed91a73838554f783882fd
David19 <davidleonel911@gmail.com>
2026-06-23 08:03:15 -0500

    crear dataframes

--- Basico/2.py
+++ Basico/2.py
@@ -5,8 +5,8 @@ df = pd.read_csv('Basico/fifa_players.csv', encoding='latin1') #latin1 para tild
 
 #print(df.isnull().sum()) #sumar todos los valores nulos de cada columna
 
-print(df['age'].mean()) 
+print(df['age'].mean()) #muestra el promedio de edad 
 
-nacionalidad = df[df['nationality'] == 'Ecuador']
+nacionalidad = df[df['nationality'] == 'Ecuador'] #creamos un dataframe, buscamos en nacionalidad sea Ecuatorianos 
 
-print(nacionalidad['name'])
+print(nacionalidad['name']) #muestras elk campo name de nuestro nuevo dataframe 
