cbbb58fd65d76d9656cdd270a4cec16a3fd3e2dc
David19 <davidleonel911@gmail.com>
2026-06-23 07:53:13 -0500

    observar nulos

--- Basico/TrabajarNulos/limpiar_nulos.py
+++ Basico/TrabajarNulos/limpiar_nulos.py
@@ -2,10 +2,10 @@
 
 import pandas as pd
 import numpy as np
-
+ 
 df = pd.read_csv('Basico/fifa_players.csv', encoding='latin1')
 
-#print(df.isnull().sum()) 
+print(df.isnull().sum()) # cuenta los nulos en cada columna 
 
-#print(df.shape) #imprimir numero de columnas y filas 
-#print(df.columns) #nombre de las columnas 
+print(df.shape) #imprimir numero de columnas y filas .....out (17954, 51)
+print(df.columns) #nombre de las columnas en un array
